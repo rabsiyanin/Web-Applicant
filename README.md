653e96a31ad922e2e96423d77c46c28eea68b547
Georgii <75013070+rabsiyanin@users.noreply.github.com>
2022-05-04 09:06:54 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Web-Applicant
+
+An application to the coursework "Разработка веб-приложения для выбора вуза абитуриентом в Нижнем Новгороде / Development of a web application for choosing a University by an applicant in Nizhny Novgorod". Its main aim is to help an applicant in choosing university after finishing high school.
+
+Main features are:
+* Browse available universities, its faculties and specialties.
+* Select the universities according to the exam results of an applicant.
+* To-do list for an applicant with an option of editing.
+
+## Installation
+
+In order to complete the installation it is better to know and have installed Node.js and node-gyp. 
+The actions below are shown as commands completed in a command prompt.
+
+1. Clone the repo: `git clone https://github.com/rabsiyanin/Web-Applicant.git`
+
+2. Install npm package with `npm install` or  `npm install --force`.
+
+3. Install ionic framework with: 
+	3.1 `npm install -g @ionic/cli`
+	3.2 `npm install @ionic/app-scripts@latest --save-dev`
+
+4. Launch the server. Open `http://localhost:8100` in your browser or run `ionic serve`.
---
+++ src/pages/about/about.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  public exams:string[]=[
+    "Русский язык",
+    "Обществознание",
+    "Математика",
+    "Биология",
+    "Английский язык",
+    "Химия",
+    "Физика",
+    "История",
+    "География",
+    "Информатика"
+  ];
+  public active:string[] = [];
+  public isToastActive:number = 0;
+
+  constructor(public toastCtrl: ToastController, public modal: ModalController) {
+  }
+
+  toggleCh(ch:string){
+    if(this.active.indexOf(ch) >= 0){
+      this.active.splice(this.active.indexOf(ch), 1);
+    }
+    else{
+      this.active.push(ch);
+    }
+  }
+  f(){
+    if(this.active.length != 3 && this.isToastActive==0) {
+      this.isToastActive = 1;
+      let toast = this.toastCtrl.create({
+        message: "Неверное количество баллов ЕГЭ",
+        duration: 15000,
+        showCloseButton: true,
+        closeButtonText: 'Исправить',
+        dismissOnPageChange: true,
+        cssClass: "Toast",
+
+      });
+      toast.onDidDismiss(()=>{
+        this.isToastActive = 0;
+      });
+      toast.present();
+    }
+    else if(this.active.length == 3 && this.isToastActive==0){
+      const myModal = this.modal.create("ModalSearchPage", {data: this.active});
+      myModal.present();
+    }
+  }
+}
